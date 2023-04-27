9ca6edaa0dd89ae91f91c577061fd23091c11645
itsaln <itsaln23@gmail.com>
2023-04-27 14:49:27 +0500

    01

--- app/api/interceptors.ts
+++ app/api/interceptors.ts
@@ -3,17 +3,10 @@ import axios from 'axios'
 import { getContentType } from '@/api/api.helpers'
 
 import { API_SERVER_URL, API_URL } from '@/config/api.config'
-
-// export const axiosClassic = axios.create({
-// 	baseURL: IS_PRODUCTION ? API_SERVER_URL : API_URL,
-// 	headers: {
-// 		'Content-Type': 'application/json',
-// 		'Accept-Encoding': 'gzip'
-// 	}
-// })
+import { IS_PRODUCTION } from '@/config/constants'
 
 export const instance = axios.create({
-	baseURL: API_URL,
+	baseURL: IS_PRODUCTION ? API_SERVER_URL : API_URL,
 	headers: getContentType()
 })
 
